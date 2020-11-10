d6629acc6b0427387f69bcccdf1abfb46218cf02
Aquib <aquibshaikh02397@gmail.com>
2020-11-10 20:27:44 +0530

    showMsg function is added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
-
+click = "";
+function showMsg() {
+  click =
+    click == "" ? (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+    ) : (
+      ""
+    );
+}
 function App() {
   return (
     <div id="main">
       // Do not alter the main div
+      <button id="click" onClick={showMsg}>
+        Click Me!
+      </button>
+      {click}
     </div>
   );
 }
-
 
 export default App;
