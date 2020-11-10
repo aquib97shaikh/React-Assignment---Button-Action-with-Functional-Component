9de696bdb86b267f7cd000b2aeadda8128969e9f
Aquib <aquibshaikh02397@gmail.com>
2020-11-10 20:46:16 +0530

    state added

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,22 @@
 import React, { Component, useState } from "react";
 import "./../styles/App.css";
-click = "";
-function showMsg() {
-  click =
-    click == "" ? (
-      <p id="para">
-        Hello, I've learnt to use the full-stack evaluation tool. This makes me
-        so happy
-      </p>
-    ) : (
-      ""
-    );
-}
+
 function App() {
+  const [click, setClick] = useState("");
+  
+  function showMsg() {
+    setClick(
+      click == "" ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : (
+        ""
+      ))
+  }
   return (
     <div id="main">
-      // Do not alter the main div
       <button id="click" onClick={showMsg}>
         Click Me!
       </button>
